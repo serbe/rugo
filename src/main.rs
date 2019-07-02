af91a0f04ec7e489682647d79189ffdc97e18b33
Valeriy Kostikov <mkoctuk@gmail.com>
2019-07-02 21:36:31 +0300

    use name_command

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use actix_web::{
     HttpServer,
 };
 use futures::Future;
+use postgres::Connection;
 use r2d2::Pool;
 use r2d2_postgres::PostgresConnectionManager;
 use serde_json::json;
@@ -24,12 +25,20 @@ fn get_manager() -> PostgresConnectionManager {
         .unwrap_or_else(|_| panic!("Error connection manager to {}", conn_url))
 }
 
-fn educations_near(
-    db: web::Data<Pool<PostgresConnectionManager>>,
+fn get_data(conn: &Connection, name: &str, command: &str) -> Result<serde_json::value::Value, serde_json::value::Value> {
+    match (name, command) {
+        ("educations", "near") => Ok(json!({"data": EducationShort::get_near(conn)})),
+        ("practices", "near") => Ok(json!({"data": PracticeShort::get_near(conn)})),
+        _ => Err(json!({}))
+    }
+}
+
+fn name_command(
+    db: web::Data<Pool<PostgresConnectionManager>>, path: web::Path<(String, String)>
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     web::block(move || {
         let conn = db.get().unwrap();
-        EducationShort::get_near(&conn)
+        get_data(&conn, &path.0, &path.1)
     })
     .then(|res| match res {
         Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
@@ -41,22 +50,39 @@ fn educations_near(
     })
 }
 
-fn practices_near(
-    db: web::Data<Pool<PostgresConnectionManager>>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let conn = db.get().unwrap();
-        PracticeShort::get_near(&conn)
-    })
-    .then(|res| match res {
-        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
-            "data": db_result,
-            "error": Null,
-            "ok": true
-        }))),
-        Err(_) => Ok(HttpResponse::InternalServerError().into()),
-    })
-}
+// fn educations_near(
+//     db: web::Data<Pool<PostgresConnectionManager>>,
+// ) -> impl Future<Item = HttpResponse, Error = Error> {
+//     web::block(move || {
+//         let conn = db.get().unwrap();
+//         EducationShort::get_near(&conn)
+//     })
+//     .then(|res| match res {
+//         Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
+//             "data": db_result,
+//             "error": Null,
+//             "ok": true
+//         }))),
+//         Err(_) => Ok(HttpResponse::InternalServerError().into()),
+//     })
+// }
+
+// fn practices_near(
+//     db: web::Data<Pool<PostgresConnectionManager>>,
+// ) -> impl Future<Item = HttpResponse, Error = Error> {
+//     web::block(move || {
+//         let conn = db.get().unwrap();
+//         PracticeShort::get_near(&conn)
+//     })
+//     .then(|res| match res {
+//         Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
+//             "data": db_result,
+//             "error": Null,
+//             "ok": true
+//         }))),
+//         Err(_) => Ok(HttpResponse::InternalServerError().into()),
+//     })
+// }
 
 fn main() -> io::Result<()> {
     let manager = get_manager();
@@ -67,14 +93,13 @@ fn main() -> io::Result<()> {
         App::new()
             .data(pool.clone())
             // .wrap(middleware::Logger::default())
-            .route(
-                "/api/go/educations/near",
-                web::get().to_async(educations_near),
+            .service(
+                web::resource("/api/go/{name}/{command}").route(web::get().to_async(name_command))
             )
-            .route(
-                "/api/go/practices/near",
-                web::get().to_async(practices_near),
-            )
+            // .route(
+            //     "/api/go/practices/near",
+            //     web::get().to_async(practices_near),
+            // )
             // .route(
             //     "/api/go/contacts/list",
             //     web::get().to_async(contacts_list),
