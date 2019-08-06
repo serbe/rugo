4f0e20dd14339e47eeb634a6233251bf30b45635
Valeriy Kostikov <mkoctuk@gmail.com>
2019-08-06 21:33:39 +0300

    add missing selects

--- src/select.rs
+++ src/select.rs
@@ -63,6 +63,31 @@ impl SelectItem {
         Ok(companies)
     }
 
+    pub fn contact_all(conn: &Connection) -> Result<Vec<SelectItem>, String> {
+        let mut contacts = Vec::new();
+            for row in &conn
+                .query(
+                    "
+                        SELECT
+                            id,
+                            name
+                        FROM
+                            contacts
+                        ORDER BY
+						    name ASC
+                    ",
+                    &[],
+                )
+                .map_err(|e| format!("contact select all {}", e.to_string()))?
+            {
+                contacts.push(SelectItem {
+                    id: row.get(0),
+                    name: row.get(1),
+                })
+            }
+            Ok(contacts)
+    }
+
     // pub fn contact(conn: &Connection, id: i64) -> Result<SelectItem, String> {
     //     let mut contact = SelectItem::new();
     //     if id == 0 {
@@ -144,30 +169,30 @@ impl SelectItem {
         Ok(departments)
     }
 
-    // pub fn kind_all(conn: &Connection) -> Result<Vec<SelectItem>, String> {
-    //     let mut kinds = Vec::new();
-    //     for row in &conn
-    //         .query(
-    //             "
-    //                 SELECT
-    //                     id,
-    //                     name
-    //                 FROM
-    //                     kinds
-    //                 ORDER BY
-    //                     name ASC
-    //             ",
-    //             &[],
-    //         )
-    //         .map_err(|e| format!("kind select all {}", e.to_string()))?
-    //     {
-    //         kinds.push(SelectItem {
-    //             id: row.get(0),
-    //             name: row.get(1),
-    //         });
-    //     }
-    //     Ok(kinds)
-    // }
+    pub fn kind_all(conn: &Connection) -> Result<Vec<SelectItem>, String> {
+        let mut kinds = Vec::new();
+        for row in &conn
+            .query(
+                "
+                    SELECT
+                        id,
+                        name
+                    FROM
+                        kinds
+                    ORDER BY
+                        name ASC
+                ",
+                &[],
+            )
+            .map_err(|e| format!("kind select all {}", e.to_string()))?
+        {
+            kinds.push(SelectItem {
+                id: row.get(0),
+                name: row.get(1),
+            });
+        }
+        Ok(kinds)
+    }
 
     // pub fn post(conn: &Connection, id: i64) -> Result<SelectItem, String> {
     //     let mut post = SelectItem::new();
@@ -250,6 +275,31 @@ impl SelectItem {
             });
         }
         Ok(posts)
+    }
+
+    pub fn practice_all(conn: &Connection) -> Result<Vec<SelectItem>, String> {
+        let mut practices = Vec::new();
+        for row in &conn
+            .query(
+                "
+                    SELECT
+                        id,
+                        name
+                    FROM
+                        practices
+                    ORDER BY
+                        name ASC
+                ",
+                &[],
+            )
+            .map_err(|e| format!("practice select all {}", e.to_string()))?
+        {
+            practices.push(SelectItem {
+                id: row.get(0),
+                name: row.get(1),
+            });
+        }
+        Ok(practices)
     }
 
     pub fn rank_all(conn: &Connection) -> Result<Vec<SelectItem>, String> {
