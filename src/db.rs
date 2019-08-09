40e2156df4a320ec98d11377bf633c6155febe74
Serbe <mkoctuk@gmail.com>
2019-08-09 16:01:12 +0300

    move code to db

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,196 @@
+use actix_web::{web, Error, HttpResponse};
 use dotenv::dotenv;
-// use postgres::Connection;
-
+use futures::Future;
+use postgres::Connection;
+use r2d2::Pool;
+use r2d2_postgres::PostgresConnectionManager;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value::Null};
 use std::env;
 
-pub fn get_connurl() -> String {
+use crate::certificate::{Certificate, CertificateList};
+use crate::company::{Company, CompanyList};
+use crate::contact::{Contact, ContactList};
+use crate::department::{Department, DepartmentList};
+use crate::education::{Education, EducationList, EducationShort};
+use crate::kind::{Kind, KindList};
+use crate::post::{Post, PostList};
+use crate::practice::{Practice, PracticeList, PracticeShort};
+use crate::rank::{Rank, RankList};
+use crate::scope::{Scope, ScopeList};
+use crate::select::SelectItem;
+use crate::siren::{Siren, SirenList};
+use crate::siren_type::{SirenType, SirenTypeList};
+
+#[derive(Deserialize, Serialize)]
+pub enum DBResult {
+    Certificate(Certificate),
+    CertificateList(Vec<CertificateList>),
+    Company(Box<Company>),
+    CompanyList(Vec<CompanyList>),
+    Contact(Box<Contact>),
+    ContactList(Vec<ContactList>),
+    Department(Department),
+    DepartmentList(Vec<DepartmentList>),
+    Education(Education),
+    EducationList(Vec<EducationList>),
+    EducationShort(Vec<EducationShort>),
+    Kind(Kind),
+    KindList(Vec<KindList>),
+    Post(Post),
+    PostList(Vec<PostList>),
+    Practice(Practice),
+    PracticeList(Vec<PracticeList>),
+    PracticeShort(Vec<PracticeShort>),
+    Rank(Rank),
+    RankList(Vec<RankList>),
+    Scope(Scope),
+    ScopeList(Vec<ScopeList>),
+    SelectItem(Vec<SelectItem>),
+    Siren(Box<Siren>),
+    SirenList(Vec<SirenList>),
+    SirenType(SirenType),
+    SirenTypeList(Vec<SirenTypeList>),
+}
+
+fn get_connurl() -> String {
     dotenv().ok();
     env::var("DATABASE_URL").expect("DATABASE_URL must be set")
 }
 
-// pub fn get_connection() -> Connection {
-//     let conn_url = get_connurl();
-//     Connection::connect(conn_url.clone(), postgres::TlsMode::None)
-//         .unwrap_or_else(|_| panic!("Error connecting to {}", conn_url))
-// }
+pub fn get_manager() -> PostgresConnectionManager {
+    let conn_url = get_connurl();
+    PostgresConnectionManager::new(conn_url.clone(), r2d2_postgres::TlsMode::None)
+        .unwrap_or_else(|_| panic!("Error connection manager to {}", conn_url))
+}
+
+fn get_list(conn: &Connection, name: &str, command: &str) -> Result<DBResult, String> {
+    match (name, command) {
+        ("certificate", "list") => Ok(DBResult::CertificateList(CertificateList::get_all(conn)?)),
+        ("company", "list") => Ok(DBResult::CompanyList(CompanyList::get_all(conn)?)),
+        ("company", "select") => Ok(DBResult::SelectItem(SelectItem::company_all(conn)?)),
+        ("contact", "list") => Ok(DBResult::ContactList(ContactList::get_all(conn)?)),
+        ("contact", "select") => Ok(DBResult::SelectItem(SelectItem::contact_all(conn)?)),
+        ("department", "list") => Ok(DBResult::DepartmentList(DepartmentList::get_all(conn)?)),
+        ("department", "select") => Ok(DBResult::SelectItem(SelectItem::department_all(conn)?)),
+        ("education", "list") => Ok(DBResult::EducationList(EducationList::get_all(conn)?)),
+        ("education", "near") => Ok(DBResult::EducationShort(EducationShort::get_near(conn)?)),
+        ("kind", "list") => Ok(DBResult::KindList(KindList::get_all(conn)?)),
+        ("kind", "select") => Ok(DBResult::SelectItem(SelectItem::kind_all(conn)?)),
+        ("post", "list") => Ok(DBResult::PostList(PostList::get_all(conn)?)),
+        ("post", "select") => Ok(DBResult::SelectItem(SelectItem::post_all(conn, false)?)),
+        ("post_go", "select") => Ok(DBResult::SelectItem(SelectItem::post_all(conn, true)?)),
+        ("practice", "list") => Ok(DBResult::PracticeList(PracticeList::get_all(conn)?)),
+        ("practice", "near") => Ok(DBResult::PracticeShort(PracticeShort::get_near(conn)?)),
+        ("rank", "list") => Ok(DBResult::RankList(RankList::get_all(conn)?)),
+        ("rank", "select") => Ok(DBResult::SelectItem(SelectItem::rank_all(conn)?)),
+        ("scope", "list") => Ok(DBResult::ScopeList(ScopeList::get_all(conn)?)),
+        ("scope", "select") => Ok(DBResult::SelectItem(SelectItem::scope_all(conn)?)),
+        ("siren", "list") => Ok(DBResult::SirenList(SirenList::get_all(conn)?)),
+        ("siren_type", "list") => Ok(DBResult::SirenTypeList(SirenTypeList::get_all(conn)?)),
+        ("siren_type", "select") => Ok(DBResult::SelectItem(SelectItem::siren_type_all(conn)?)),
+        _ => Err("bad path".to_string()),
+    }
+}
+
+fn get_item(conn: &Connection, name: &str, id: i64) -> Result<DBResult, String> {
+    match name {
+        "certificate" => Ok(DBResult::Certificate(Certificate::get(conn, id)?)),
+        "company" => Ok(DBResult::Company(Box::new(Company::get(conn, id)?))),
+        "contact" => Ok(DBResult::Contact(Box::new(Contact::get(conn, id)?))),
+        "department" => Ok(DBResult::Department(Department::get(conn, id)?)),
+        "education" => Ok(DBResult::Education(Education::get(conn, id)?)),
+        "kind" => Ok(DBResult::Kind(Kind::get(conn, id)?)),
+        "post" => Ok(DBResult::Post(Post::get(conn, id)?)),
+        "practice" => Ok(DBResult::Practice(Practice::get(conn, id)?)),
+        "rank" => Ok(DBResult::Rank(Rank::get(conn, id)?)),
+        "scope" => Ok(DBResult::Scope(Scope::get(conn, id)?)),
+        "siren" => Ok(DBResult::Siren(Box::new(Siren::get(conn, id)?))),
+        "siren_type" => Ok(DBResult::SirenType(SirenType::get(conn, id)?)),
+        _ => Err("bad path".to_string()),
+    }
+}
+
+fn get_children(
+    conn: &Connection,
+    name: &str,
+    children: &str,
+    id: i64,
+) -> Result<DBResult, String> {
+    match (name, children) {
+        // "certificate" => Ok(DBResult::Certificate(Certificate::get(conn, id)?)),
+        ("company", "practice") => Ok(DBResult::PracticeList(PracticeList::get_by_company(
+            conn, id,
+        )?)),
+        // "contact" => Ok(DBResult::Contact(Box::new(Contact::get(conn, id)?))),
+        // "department" => Ok(DBResult::Department(Department::get(conn, id)?)),
+        // "education" => Ok(DBResult::Education(Education::get(conn, id)?)),
+        // "kind" => Ok(DBResult::Kind(Kind::get(conn, id)?)),
+        // "post" => Ok(DBResult::Post(Post::get(conn, id)?)),
+        // "practice" => Ok(DBResult::Practice(Practice::get(conn, id)?)),
+        _ => Err("bad path".to_string()),
+    }
+}
+
+pub fn name_children(
+    db: web::Data<Pool<PostgresConnectionManager>>,
+    path: web::Path<(String, String, i64)>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let conn = db.get().unwrap();
+        get_children(&conn, &path.0, &path.1, path.2)
+    })
+    .then(|res| match res {
+        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
+            "data": db_result,
+            "error": Null,
+            "ok": true
+        }))),
+        Err(err) => {
+            println!("{}", err);
+            Ok(HttpResponse::InternalServerError().into())
+        }
+    })
+}
+
+pub fn name_command(
+    db: web::Data<Pool<PostgresConnectionManager>>,
+    path: web::Path<(String, String)>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let conn = db.get().unwrap();
+        get_list(&conn, &path.0, &path.1)
+    })
+    .then(|res| match res {
+        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
+            "data": db_result,
+            "error": Null,
+            "ok": true
+        }))),
+        Err(err) => {
+            println!("{}", err);
+            Ok(HttpResponse::InternalServerError().into())
+        }
+    })
+}
+
+pub fn name_id(
+    db: web::Data<Pool<PostgresConnectionManager>>,
+    path: web::Path<(String, i64)>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let conn = db.get().unwrap();
+        get_item(&conn, &path.0, path.1)
+    })
+    .then(|res| match res {
+        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
+            "data": db_result,
+            "error": Null,
+            "ok": true
+        }))),
+        Err(err) => {
+            println!("{}", err);
+            Ok(HttpResponse::InternalServerError().into())
+        }
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
-use actix_identity::{CookieIdentityPolicy, IdentityService};
-use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
-use futures::Future;
-use postgres::Connection;
-use r2d2::Pool;
-use r2d2_postgres::PostgresConnectionManager;
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value::Null};
+// use actix_identity::{CookieIdentityPolicy, IdentityService};
+// use actix_session::{Session, CookieSession};
+use actix_web::{middleware, web, App, HttpServer};
 use std::io;
 
-use db::get_connurl;
-
-use certificate::{Certificate, CertificateList};
-use company::{Company, CompanyList};
-use contact::{Contact, ContactList};
-use department::{Department, DepartmentList};
-use education::{Education, EducationList, EducationShort};
-use kind::{Kind, KindList};
-use post::{Post, PostList};
-use practice::{Practice, PracticeList, PracticeShort};
-use rank::{Rank, RankList};
-use scope::{Scope, ScopeList};
-use select::SelectItem;
-use siren::{Siren, SirenList};
-use siren_type::{SirenType, SirenTypeList};
+use db::{get_manager, name_command, name_children, name_id};
 
 mod certificate;
 mod company;
@@ -42,180 +23,12 @@ mod siren;
 mod siren_type;
 mod tcc;
 
-// mod models;
-
-#[derive(Deserialize, Serialize)]
-enum DBResult {
-    Certificate(Certificate),
-    CertificateList(Vec<CertificateList>),
-    Company(Box<Company>),
-    CompanyList(Vec<CompanyList>),
-    Contact(Box<Contact>),
-    ContactList(Vec<ContactList>),
-    Department(Department),
-    DepartmentList(Vec<DepartmentList>),
-    Education(Education),
-    EducationList(Vec<EducationList>),
-    EducationShort(Vec<EducationShort>),
-    Kind(Kind),
-    KindList(Vec<KindList>),
-    Post(Post),
-    PostList(Vec<PostList>),
-    Practice(Practice),
-    PracticeList(Vec<PracticeList>),
-    PracticeShort(Vec<PracticeShort>),
-    Rank(Rank),
-    RankList(Vec<RankList>),
-    Scope(Scope),
-    ScopeList(Vec<ScopeList>),
-    SelectItem(Vec<SelectItem>),
-    Siren(Box<Siren>),
-    SirenList(Vec<SirenList>),
-    SirenType(SirenType),
-    SirenTypeList(Vec<SirenTypeList>),
-}
-
-fn get_manager() -> PostgresConnectionManager {
-    let conn_url = get_connurl();
-    PostgresConnectionManager::new(conn_url.clone(), r2d2_postgres::TlsMode::None)
-        .unwrap_or_else(|_| panic!("Error connection manager to {}", conn_url))
-}
-
-fn get_list(conn: &Connection, name: &str, command: &str) -> Result<DBResult, String> {
-    match (name, command) {
-        ("certificate", "list") => Ok(DBResult::CertificateList(CertificateList::get_all(conn)?)),
-        ("company", "list") => Ok(DBResult::CompanyList(CompanyList::get_all(conn)?)),
-        ("company", "select") => Ok(DBResult::SelectItem(SelectItem::company_all(conn)?)),
-        ("contact", "list") => Ok(DBResult::ContactList(ContactList::get_all(conn)?)),
-        ("contact", "select") => Ok(DBResult::SelectItem(SelectItem::contact_all(conn)?)),
-        ("department", "list") => Ok(DBResult::DepartmentList(DepartmentList::get_all(conn)?)),
-        ("department", "select") => Ok(DBResult::SelectItem(SelectItem::department_all(conn)?)),
-        ("education", "list") => Ok(DBResult::EducationList(EducationList::get_all(conn)?)),
-        ("education", "near") => Ok(DBResult::EducationShort(EducationShort::get_near(conn)?)),
-        ("kind", "list") => Ok(DBResult::KindList(KindList::get_all(conn)?)),
-        ("kind", "select") => Ok(DBResult::SelectItem(SelectItem::kind_all(conn)?)),
-        ("post", "list") => Ok(DBResult::PostList(PostList::get_all(conn)?)),
-        ("post", "select") => Ok(DBResult::SelectItem(SelectItem::post_all(conn, false)?)),
-        ("post_go", "select") => Ok(DBResult::SelectItem(SelectItem::post_all(conn, true)?)),
-        ("practice", "list") => Ok(DBResult::PracticeList(PracticeList::get_all(conn)?)),
-        ("practice", "near") => Ok(DBResult::PracticeShort(PracticeShort::get_near(conn)?)),
-        ("rank", "list") => Ok(DBResult::RankList(RankList::get_all(conn)?)),
-        ("rank", "select") => Ok(DBResult::SelectItem(SelectItem::rank_all(conn)?)),
-        ("scope", "list") => Ok(DBResult::ScopeList(ScopeList::get_all(conn)?)),
-        ("scope", "select") => Ok(DBResult::SelectItem(SelectItem::scope_all(conn)?)),
-        ("siren", "list") => Ok(DBResult::SirenList(SirenList::get_all(conn)?)),
-        ("siren_type", "list") => Ok(DBResult::SirenTypeList(SirenTypeList::get_all(conn)?)),
-        ("siren_type", "select") => Ok(DBResult::SelectItem(SelectItem::siren_type_all(conn)?)),
-        _ => Err("bad path".to_string()),
-    }
-}
-
-fn get_item(conn: &Connection, name: &str, id: i64) -> Result<DBResult, String> {
-    match name {
-        "certificate" => Ok(DBResult::Certificate(Certificate::get(conn, id)?)),
-        "company" => Ok(DBResult::Company(Box::new(Company::get(conn, id)?))),
-        "contact" => Ok(DBResult::Contact(Box::new(Contact::get(conn, id)?))),
-        "department" => Ok(DBResult::Department(Department::get(conn, id)?)),
-        "education" => Ok(DBResult::Education(Education::get(conn, id)?)),
-        "kind" => Ok(DBResult::Kind(Kind::get(conn, id)?)),
-        "post" => Ok(DBResult::Post(Post::get(conn, id)?)),
-        "practice" => Ok(DBResult::Practice(Practice::get(conn, id)?)),
-        "rank" => Ok(DBResult::Rank(Rank::get(conn, id)?)),
-        "scope" => Ok(DBResult::Scope(Scope::get(conn, id)?)),
-        "siren" => Ok(DBResult::Siren(Box::new(Siren::get(conn, id)?))),
-        "siren_type" => Ok(DBResult::SirenType(SirenType::get(conn, id)?)),
-        _ => Err("bad path".to_string()),
-    }
-}
-
-fn get_children(
-    conn: &Connection,
-    name: &str,
-    children: &str,
-    id: i64,
-) -> Result<DBResult, String> {
-    match (name, children) {
-        // "certificate" => Ok(DBResult::Certificate(Certificate::get(conn, id)?)),
-        ("company", "practice") => Ok(DBResult::PracticeList(PracticeList::get_by_company(
-            conn, id,
-        )?)),
-        // "contact" => Ok(DBResult::Contact(Box::new(Contact::get(conn, id)?))),
-        // "department" => Ok(DBResult::Department(Department::get(conn, id)?)),
-        // "education" => Ok(DBResult::Education(Education::get(conn, id)?)),
-        // "kind" => Ok(DBResult::Kind(Kind::get(conn, id)?)),
-        // "post" => Ok(DBResult::Post(Post::get(conn, id)?)),
-        // "practice" => Ok(DBResult::Practice(Practice::get(conn, id)?)),
-        _ => Err("bad path".to_string()),
-    }
-}
-
-fn name_children(
-    db: web::Data<Pool<PostgresConnectionManager>>,
-    path: web::Path<(String, String, i64)>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let conn = db.get().unwrap();
-        get_children(&conn, &path.0, &path.1, path.2)
-    })
-    .then(|res| match res {
-        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
-            "data": db_result,
-            "error": Null,
-            "ok": true
-        }))),
-        Err(err) => {
-            println!("{}", err);
-            Ok(HttpResponse::InternalServerError().into())
-        }
-    })
-}
-
-fn name_command(
-    db: web::Data<Pool<PostgresConnectionManager>>,
-    path: web::Path<(String, String)>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let conn = db.get().unwrap();
-        get_list(&conn, &path.0, &path.1)
-    })
-    .then(|res| match res {
-        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
-            "data": db_result,
-            "error": Null,
-            "ok": true
-        }))),
-        Err(err) => {
-            println!("{}", err);
-            Ok(HttpResponse::InternalServerError().into())
-        }
-    })
-}
-
-fn name_id(
-    db: web::Data<Pool<PostgresConnectionManager>>,
-    path: web::Path<(String, i64)>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let conn = db.get().unwrap();
-        get_item(&conn, &path.0, path.1)
-    })
-    .then(|res| match res {
-        Ok(db_result) => Ok(HttpResponse::Ok().json(json!({
-            "data": db_result,
-            "error": Null,
-            "ok": true
-        }))),
-        Err(err) => {
-            println!("{}", err);
-            Ok(HttpResponse::InternalServerError().into())
-        }
-    })
-}
-
 fn main() -> io::Result<()> {
     let manager = get_manager();
     let pool = r2d2::Pool::new(manager).unwrap();
     let sys = actix_rt::System::new("rugo");
+    // let secret_key = dotenv::var("SECRET_KEY").unwrap();
+    // let cookie_secret = base64::decode(&secret_key).unwrap();
 
     std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
     env_logger::init();
@@ -224,14 +37,18 @@ fn main() -> io::Result<()> {
         App::new()
             .data(pool.clone())
             .wrap(middleware::Logger::default())
-            .wrap(IdentityService::new(
-                CookieIdentityPolicy::new("SECRET_KEY".as_bytes())
-                    .name("auth")
-                    .path("/")
-                    .domain("localhost")
-                    .max_age_time(chrono::Duration::days(1))
-                    .secure(false), // this can only be true if you have https
-            ))
+            // .wrap(
+            //   CookieSession::signed(&[0; 32]) // <- create cookie based session middleware
+            //         .secure(false)
+            //  )
+            // .wrap(IdentityService::new(
+            //     CookieIdentityPolicy::new(secret_key.as_bytes())
+            //         .name("auth")
+            //         .path("/")
+            //         .domain("localhost")
+            //         .max_age_time(chrono::Duration::days(1))
+            //         .secure(false), // this can only be true if you have https
+            // ))
             // limit the maximum amount of data that server will accept
             .data(web::JsonConfig::default().limit(4096))
             .service(
